62f404c2d09f40d122fe439041597fed07e15837
Zhaobo Hu <zhaobohu2002@gmail.com>
2024-12-09 17:11:27 -0600

    Update step.R

--- analysis/step.R
+++ analysis/step.R
@@ -1,10 +1,9 @@
 
-
+# model
 dat <- read.csv("../results/vertebrates/parsed.csv")
 dat <- dat[!duplicated(dat$species), ]
 
-d <- na.omit(dat[, c("rsq", "clade", "median", "totalrep.pct")])
-d$median <- 70 - d$median # subtract median by max divergence
+d <- na.omit(dat[, c("species", "rsq", "clade", "median", "totalrep.pct")])
 summary(step(glm(d$rsq ~ d$median * d$totalrep.pct)))
 
 m <- d[d$clade == "Mammalia", ]
@@ -16,36 +15,33 @@ summary(step(glm(f$rsq ~ f$median * f$totalrep.pct)))
 r <- d[d$clade == "Sauria", ]
 summary(step(glm(r$rsq ~ r$median * r$totalrep.pct)))
 
+# test for phylogenetic signal
+library(phytools)
+tree <- read.tree("../data/vertebrates/formatted_tree.nwk")
+tree$tip.label <- gsub("_", " ", tree$tip.label)
+int <- intersect(d$species, tree$tip.label)
+pruned.tree <- keep.tip(tree, int)
+res <- setNames(resid(step(glm(d$rsq ~ d$median:d$totalrep.pct))), d$species)
+phylosig(pruned.tree, res, method = "lambda", test = TRUE)
 
+# PGLS model
+library(caper)
+cd <- comparative.data(tree, d, species)
+pgls.model <- pgls(rsq ~ median + totalrep.pct + median : totalrep.pct, data = cd)
+summary(pgls.model)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-permTest <- function(x, y, reps, method) {
-  perm.y <- replicate(reps, sample(y))
-  perm.cor <- apply(perm.y, MARGIN = 2, function(col) cor(x, col, method = method))
-  pval <- mean(abs(perm.cor) >= abs(cor(x, y, method = method)))
-  return(pval)
-}
-
-y <- d$rsq
-x <- d$median * d$totalrep.pct
-r <- signif(cor(x, y, method = "pearson"), 3)
-perm.pval <- signif(permTest(x, y, 100000, "pearson"), 3)
-
-
-
-
-
+# compare AICs
+d <- d[d$species %in% int, ]
+summary(gls(rsq ~ median + totalrep.pct + median:totalrep.pct, 
+    data = d, 
+    correlation = corBrownian(phy = pruned.tree, form = ~species),
+    method = "ML")) # 21.78158
+summary(gls(rsq ~ median, 
+            data = d, 
+            correlation = corBrownian(phy = pruned.tree, form = ~species),
+            method = "ML")) # 23.40565
+summary(gls(rsq ~ totalrep.pct, 
+            data = d, 
+            correlation = corBrownian(phy = pruned.tree, form = ~species),
+            method = "ML")) # 25.3864
 
