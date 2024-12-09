3c85ed10c8fd438bc13e3b7718703c75ca42bd2e
Zhaobo Hu <zhaobohu2002@gmail.com>
2024-12-09 12:54:15 -0600

    Update model

--- analysis/step.R
+++ analysis/step.R
@@ -4,30 +4,17 @@ dat <- read.csv("../results/vertebrates/parsed.csv")
 dat <- dat[!duplicated(dat$species), ]
 
 d <- na.omit(dat[, c("rsq", "clade", "median", "totalrep.pct")])
-summary(step(glm(d$rsq ~ d$median : d$totalrep.pct)))
-int <- d$median * d$totalrep.pct
-plot(d$rsq ~ int)
-abs(diff(range(int))*summary(glm(d$rsq ~ d$median : d$totalrep.pct))$coefficients[2, 1]/diff(range(d$rsq)))
+d$median <- 70 - d$median # subtract median by max divergence
+summary(step(glm(d$rsq ~ d$median * d$totalrep.pct)))
 
 m <- d[d$clade == "Mammalia", ]
-summary(glm(m$rsq ~ m$median : m$totalrep.pct))
-int <- m$median * m$totalrep.pct
-plot(m$rsq ~ int)
-abs(diff(range(int))*summary(glm(m$rsq ~ m$median : m$totalrep.pct))$coefficients[2, 1]/diff(range(m$rsq)))
+summary(step(glm(m$rsq ~ m$median * m$totalrep.pct)))
 
 f <- d[d$clade == "Actinopterygii", ]
-summary(glm(f$rsq ~ f$median : f$totalrep.pct))
-int <- f$median * f$totalrep.pct
-plot(f$rsq ~ int)
-abs(diff(range(int))*summary(glm(f$rsq ~ f$median : f$totalrep.pct))$coefficients[2, 1]/diff(range(f$rsq)))
+summary(step(glm(f$rsq ~ f$median * f$totalrep.pct)))
 
 r <- d[d$clade == "Sauria", ]
-summary(glm(r$rsq ~ r$median : r$totalrep.pct))
-int <- r$median * r$totalrep.pct
-plot(r$rsq ~ int)
-abs(diff(range(int))*summary(glm(r$rsq ~ r$median : r$totalrep.pct))$coefficients[2, 1]/diff(range(r$rsq)))
-
-
+summary(step(glm(r$rsq ~ r$median * r$totalrep.pct)))
 
 
 
@@ -56,5 +43,9 @@ y <- d$rsq
 x <- d$median * d$totalrep.pct
 r <- signif(cor(x, y, method = "pearson"), 3)
 perm.pval <- signif(permTest(x, y, 100000, "pearson"), 3)
+
+
+
+
 
 
