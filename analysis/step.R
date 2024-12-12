d4b6c54a3d8331f97080e9a1b2a4c236a9010511
Zhaobo Hu <zhaobohu2002@gmail.com>
2024-12-12 09:57:56 -0600

    Update step.R

--- analysis/step.R
+++ analysis/step.R
@@ -1,15 +1,53 @@
 
 
 
-
 # transform results
 dat <- read.csv("../results/vertebrates/parsed.csv")
 dat <- dat[!duplicated(dat$species), ]
+dat <- dat[!is.na(dat$chromnum.1n), ]
 dat$median.trans <- 1 - (dat$median/70)
 dat$totalrep.prop <- dat$totalrep.pct * 0.01
 
-# subset data and perform stepwise model selection
-d <- na.omit(dat[, c("species", "rsq", "clade", "median.trans", "totalrep.prop")])
+# subset data and parse assembly size by 10% of genome size
+dat <- na.omit(dat[, c("species", "rsq", "clade", "median.trans", "totalrep.prop", "asmblysize.Mbp", "est.gnsz.Mbp")])
+dat <- dat[dat$asmblysize.Mbp/dat$est.gnsz.Mbp < 1.1, ]
+dat <- dat[dat$asmblysize.Mbp/dat$est.gnsz.Mbp > 0.9, ]
+
+mam <- dat[dat$clade %in% "Mammalia", ]
+plot(mam$median.trans, mam$rsq)
+abline(glm(mam$rsq ~ mam$median.trans)) # higher score > lower divergence > newer repeats > more variation > lower rsq
+plot(mam$totalrep.prop, mam$rsq)
+abline(glm(mam$rsq ~ mam$totalrep.prop)) # more repeats > more variation > lower rsq
+plot(mam$median.trans*mam$totalrep.prop, mam$rsq)
+term <- mam$median.trans * mam$totalrep.prop
+abline(glm(mam$rsq ~ term))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# step
 summary(step(glm(d$rsq ~ d$median.trans * d$totalrep.prop))) # d$rsq ~ d$median.trans * d$totalrep.prop
 
 # stepwise selection for each clade
@@ -48,11 +86,3 @@ summary(pgls(rsq ~ median.trans, data = cd))
 res <- setNames(resid(glm(m$rsq ~ m$median.trans * m$totalrep.prop)), m$species)
 phylosig(pruned.tree, res, method = "lambda", test = TRUE) # mammals
 summary(pgls(rsq ~ totalrep.prop * median.trans, data = cd))
-
-# PGLS for fish despite lack of phylogenetic signal
-res <- setNames(resid(glm(f$rsq ~ f$median.trans * f$totalrep.prop)), f$species)
-phylosig(pruned.tree, res, method = "lambda", test = TRUE)
-cd <- comparative.data(tree, f, species)
-summary(pgls(rsq ~ totalrep.prop * median.trans, data = cd))
-
-
