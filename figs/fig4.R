0f2642b42c2e7712f8ea260aa8bcd78ee30242dc
Zhaobo Hu <zhaobohu2002@gmail.com>
2025-03-11 12:22:51 -0500

    colour bar

--- figs/fig4.R
+++ figs/fig4.R
@@ -29,23 +29,64 @@ labels <- c("DNA age", "DNA proportion", "DNA interaction",
            "LINE age", "LINE proportion", "LINE interaction", 
            "LTR age", "LTR proportion", "LTR interaction", 
            "SINE age", "SINE proportion", "SINE interaction", 
-           "UnID age", "UnID proportion", "UnID interaction", 
+           "Unidtf age", "Unidtf proportion", "Unidtf interaction", 
            "Other age", "Other proportion", "Other interaction" 
 )
 
-# plot
-par(mar = c(8, 4, 4, 2))
+par(oma = c(0, 0, 3, 0))
+layout(matrix(1:2, ncol = 2), widths = c(4, 1)) # make 2 plots
+
+# main plot
+par(mar = c(8, 4, 1, 0))
 plot(y = est, x = x, type = "n", ylim = 1.05 * range(int), 
      ylab = "Parameter estimate", xlab = NA, axes = FALSE,
-     main = "Parameter estimates for averaged model of all species", 
-     xlim = c(0.75, length(terms)+0.2))
-abline(h = 0, lty = 2, col = "gray") # dashed line at y = 0
+     xlim = c(0.75, length(terms) + 0.2)) # plot
+abline(h = 0, lty = 2, col = "gray") # line at y = 0
 segments(x, int[, 1], x, int[, 2], lwd = 2) # confidence bars
 points(x, est, pch = 16, col = cols) # colored points
 axis(2) # y axis
 axis(1, at = x, labels = labels, las = 2) # x axis
-box() # box
+box()
+
+# color bar
+par(mar = c(8, 1, 1, 4))
+height <- seq(min(imp), max(imp), length.out = res + 1) # y values
+z <- matrix(seq(min(imp), max(imp), length.out = res), nrow = 1, ncol = res) # color gradient
+image(x = c(0, 1), y = height, z = z, col = palette, 
+      axes = FALSE, xlab = "", ylab = "") # make color bar
+ticks <- seq(min(imp), max(imp), length.out = 5) # ticks
+axis(4, at = ticks, labels = round(ticks, 2), las = 1) # y axis
+
+# title
+mtext("Parameter estimates for averaged model of all species", 
+      outer = TRUE, cex = 1.1, line = 0, font = 2, family = "sans", 
+      adj = 0.35)
 par(mar = c(5, 4, 4, 2) + 0.1) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -79,23 +120,86 @@ labels <- c("DNA age", "DNA proportion", "DNA interaction",
             "LINE age", "LINE proportion", "LINE interaction", 
             "LTR age", "LTR proportion", "LTR interaction", 
             "SINE age", "SINE proportion", "SINE interaction", 
-            "UnID age", "UnID proportion", "UnID interaction", 
+            "Unidtf age", "Unidtf proportion", "Unidtf interaction", 
             "Other age", "Other proportion", "Other interaction" 
 )
 
-# plot
-par(mar = c(8, 4, 4, 2))
+par(oma = c(0, 0, 3, 0))
+layout(matrix(1:2, ncol = 2), widths = c(4, 1)) # make 2 plots
+
+# main plot
+par(mar = c(8, 4, 1, 0))
 plot(y = est, x = x, type = "n", ylim = 1.05 * range(int), 
      ylab = "Parameter estimate", xlab = NA, axes = FALSE,
-     main = "Parameter estimates for averaged model of mammals", 
-     xlim = c(0.75, length(terms)+0.2))
-abline(h = 0, lty = 2, col = "gray") # dashed line at y = 0
+     xlim = c(0.75, length(terms) + 0.2)) # plot
+abline(h = 0, lty = 2, col = "gray") # line at y = 0
 segments(x, int[, 1], x, int[, 2], lwd = 2) # confidence bars
 points(x, est, pch = 16, col = cols) # colored points
 axis(2) # y axis
 axis(1, at = x, labels = labels, las = 2) # x axis
-box() # box
+box()
+
+# color bar
+par(mar = c(8, 1, 1, 4))
+height <- seq(min(imp), max(imp), length.out = res + 1) # y values
+z <- matrix(seq(min(imp), max(imp), length.out = res), nrow = 1, ncol = res) # color gradient
+image(x = c(0, 1), y = height, z = z, col = palette, 
+      axes = FALSE, xlab = "", ylab = "") # make color bar
+ticks <- seq(min(imp), max(imp), length.out = 5) # ticks
+axis(4, at = ticks, labels = round(ticks, 2), las = 1) # y axis
+
+# title
+mtext("Parameter estimates for averaged model of mammals", 
+      outer = TRUE, cex = 1.1, line = 0, font = 2, family = "sans", 
+      adj = 0.35)
 par(mar = c(5, 4, 4, 2) + 0.1) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -129,20 +233,36 @@ labels <- c("DNA age", "DNA proportion", "DNA interaction",
             "LINE age", "LINE proportion", "LINE interaction", 
             "LTR age", "LTR proportion", "LTR interaction", 
             "SINE age", "SINE proportion", "SINE interaction", 
-            "UnID age", "UnID proportion", "UnID interaction", 
+            "Unidtf age", "Unidtf proportion", "Unidtf interaction", 
             "Other age", "Other proportion", "Other interaction" 
 )
 
-# plot
-par(mar = c(8, 4, 4, 2))
+par(oma = c(0, 0, 3, 0))
+layout(matrix(1:2, ncol = 2), widths = c(4, 1)) # make 2 plots
+
+# main plot
+par(mar = c(8, 4, 1, 0))
 plot(y = est, x = x, type = "n", ylim = 1.05 * range(int), 
      ylab = "Parameter estimate", xlab = NA, axes = FALSE,
-     main = "Parameter estimates for averaged model of ray-finned fish", 
-     xlim = c(0.75, length(terms)+0.2))
-abline(h = 0, lty = 2, col = "gray") # dashed line at y = 0
+     xlim = c(0.75, length(terms) + 0.2)) # plot
+abline(h = 0, lty = 2, col = "gray") # line at y = 0
 segments(x, int[, 1], x, int[, 2], lwd = 2) # confidence bars
 points(x, est, pch = 16, col = cols) # colored points
 axis(2) # y axis
 axis(1, at = x, labels = labels, las = 2) # x axis
-box() # box
+box()
+
+# color bar
+par(mar = c(8, 1, 1, 4))
+height <- seq(min(imp), max(imp), length.out = res + 1) # y values
+z <- matrix(seq(min(imp), max(imp), length.out = res), nrow = 1, ncol = res) # color gradient
+image(x = c(0, 1), y = height, z = z, col = palette, 
+      axes = FALSE, xlab = "", ylab = "") # make color bar
+ticks <- seq(min(imp), max(imp), length.out = 5) # ticks
+axis(4, at = ticks, labels = round(ticks, 2), las = 1) # y axis
+
+# title
+mtext("Parameter estimates for averaged model of ray-finned fish", 
+      outer = TRUE, cex = 1.1, line = 0, font = 2, family = "sans", 
+      adj = 0.35)
 par(mar = c(5, 4, 4, 2) + 0.1) 
